baa4db29a419b3cc57952671214721b50b569738
Jelon <jelon94@gmail.com>
2017-02-15 21:25:03 +0100

    Course finished.

--- state8.js
+++ state8.js
@@ -1,9 +1,44 @@
+var text;
+
+WebFontConfig = {
+    google: { families: [ 'Candal', 'Montserrat' ]}
+};
+
 demo.state8 = function(){};
 demo.state8.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js');
+    },
     create: function(){
         game.stage.backgroundColor = '#99e6e6';
         addChangeStateEventListeners();
+        
+        text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed sagittis leo. Mauris dolor ex, imperdiet id tempus vitae, convallis sit amet massa. Morbi sem eros, vehicula imperdiet lorem vehicula, euismod fermentum elit. Vestibulum in nulla nec sapien aliquam ornare vel vitae quam. Vivamus tincidunt ut metus vel posuere. Nam mi arcu, luctus et risus vel, molestie semper felis. Nulla facilisi. Phasellus aliquet nisi non ipsum dapibus, vitae rutrum risus suscipit.';
+    
+        this.spellOutText(100, 100, 1000, text, 40, 40, '#fff', 'Candal');
+        this.spellOutText(100, 600, 1000, text, 40, 20, '#000', 'Montserrat');
     },
-    update: function(){}
+    update: function(){},
+    spellOutText: function(x, y, width, text, fontSize, speed, fill, font){
+        var sentence = game.add.text(x, y, '', {fontSize: fontSize + 'px', fill: fill, font: font});
+        var currentLine = game.add.text(10, 10, '', {fontSize: fontSize + 'px', font: font});
+        currentLine.alpha = 0;
+        var loop = game.time.events.loop(speed, addChar);
+        
+        var index = 0;
+        
+        function addChar(){
+            sentence.text += text[index];
+            currentLine.text += text[index];
+            if(currentLine.width > width && text[index] == ' '){
+                sentence.text += '\n';
+                currentLine.text = '';
+            }
+            if(index >= text.length - 1){
+                game.time.events.remove(loop);
+                console.log('stop');
+            }
+            index++;
+        }
+    }
 };
--- state9.js
+++ state9.js
@@ -1,9 +1,65 @@
-demo.state9 = function(){};
+var ref, fbObj, hsText = [];
+demo.state9 = function () {};
 demo.state9.prototype = {
-    preload: function(){},
-    create: function(){
+    preload: function () {
+        game.load.image('button1', 'assets/sprites/button1.png');
+        game.load.image('button2', 'assets/sprites/button2.png');
+    }, 
+    create: function () {
         game.stage.backgroundColor = '#ffcc66';
         addChangeStateEventListeners();
+        var config = {
+            apiKey: "key_goes_here",
+            authDomain: "auth_domain_goes_here",
+            databaseURL: "database_url_goes_here",
+            storageBucket: "storage_bucket_goes_here",
+            messagingSenderId: "sender_id_goes_here"
+        };
+        firebase.initializeApp(config);
+        firebase.auth().signInWithEmailAndPassword('mail', 'pass').catch(function(error) {
+            if(error){
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log('error!');
+            } else{
+                console.log('logged in.');
+            }
+        });       
+        ref = firebase.database().ref();
+        
+        for (var i = 1; i < 11; i++) {
+            game.add.text(500, 20 + (i * 90), i + '. ', {
+                fontSize: '40px'
+            }).anchor.setTo(1, 0);
+        }
+        for (var i = 0; i < 10; i++) {
+            hsText[i] = game.add.text(500, 20 + ((i + 1) * 90), '', {
+                fontSize: '40px'
+            });
+        }
+        var updateHSText = this.updateHSText;
+        ref.on('value', function(snapshot){
+            fbObj = snapshot.val();
+            updateHSText(fbObj.hs);
+        });
+        
+        game.add.button(800, 400, 'button1', function(){
+            var score = Math.round(Math.random() * 100);
+            fbObj.hs.push(score);
+            fbObj.hs = fbObj.hs.sort(function(a,b){
+                return b-a;
+            }).slice(0, 10);
+            ref.set(fbObj);
+        });
+        
+        game.add.button(800, 600, 'button2', function(){
+            ref.set({hs: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]});
+        });
     },
-    update: function(){}
+    update: function () {},
+    updateHSText: function(hs){
+        for (var i = 0; i < 10; i++) {
+            hsText[i].text = hs[i];
+        }
+    }
 };
